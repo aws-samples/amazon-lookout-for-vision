772f5ace1a7ae61f122708e97d6ee76937d2e7e1
taturabe <taturabe@amazon.co.jp>
2022-08-15 17:28:21 +0900

    fix typo

--- edge/install-ec2-ubuntu-deps.sh
+++ edge/install-ec2-ubuntu-deps.sh
@@ -2,7 +2,7 @@
 sudo apt install cmake
 sudo apt install python3
 sudo apt install python3-pip
-audo apt install awscli
+sudo apt install awscli
 sudo apt install software-properties-common
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
 sudo apt update
